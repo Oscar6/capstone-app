18a445fae71ea0980b27c71526df9f7c299c55ca
Oscar6 <oscar.m1024@gmail.com>
2019-03-11 18:03:00 -0500

    added popup to markers

--- src/components/DriverMap.js
+++ src/components/DriverMap.js
@@ -29,7 +29,7 @@ class DriverMap extends React.Component {
         easeLinearity={0.35}
       >
         <TileLayer url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}" />
-        <Marker position={[29.7316085, -95.4649623]} icon={storeIcon}>
+        <Marker position={[29.7401266, -95.3377017]} icon={storeIcon}>
           <Popup>
             Pick Up: <br />
             DigitalCrafts <br />
