549aec7b1919fbd65099c874ceb042e522a8ef83
Rigo-Hernandez <hernandez_rigoberto@outlook.com>
2019-03-12 14:18:35 -0500

    Added Facebook Authentication

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Card, CardBody, CardGroup, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+import Facebook from '../components/Facebook'
 
 class Login extends Component {
   render() {
@@ -35,6 +36,7 @@ class Login extends Component {
                         <Col xs="6">
                           <Button color="primary" className="px-4">Login</Button>
                         </Col>
+                        <Facebook />
                         <Col xs="6" className="text-right">
                           <Button color="link" className="px-0">Forgot password?</Button>
                         </Col>
