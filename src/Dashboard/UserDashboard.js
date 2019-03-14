aefc1937bddb409e78d59541e537c53429c69932
Oscar6 <oscar.m1024@gmail.com>
2019-03-14 10:15:36 -0500

    merging with Rigo branch

--- src/Dashboard/UserDashboard.js
+++ src/Dashboard/UserDashboard.js
@@ -4,7 +4,7 @@ import { Button } from 'react-bootstrap'
 import '../Styles/styles.css';
 import Map from '../components/Map';
 import OrderForm from '../components/OrderForm';
-import ReviewForm from '../components/ReviewForm';
+// import ReviewForm from '../components/ReviewForm';
 
 class UserDashboard extends Component {
   constructor(props) {
@@ -21,7 +21,7 @@ class UserDashboard extends Component {
         <Row className="rowContainer">
          <Col><OrderForm  /></Col>
          <Col><Map  /></Col>
-         <Col><ReviewForm  /></Col>
+         {/* <Col><ReviewForm  /></Col> */}
         </Row>
         </Container>
     );
