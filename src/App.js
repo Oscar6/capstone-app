7b309f0454be1311004324284ea490daccadf51a
Oscar6 <oscar.m1024@gmail.com>
2019-03-28 20:47:49 -0500

    background and logo updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Button } from "react-bootstrap";
 import "./Styles/styles.css";
-
+import Img from "./assets/mule-logo.png";
 // test comment
 
 class App extends Component {
@@ -17,12 +17,23 @@ class App extends Component {
     return (
       <div className="AppHeader">
         <div className="descriptionHome">
-          <p>Mule</p>
-          <p>An on-demand, hassle-free, item return service that saves you the time and
-          headache from traffic.</p>
+          <img
+            src={Img}
+            width="50%"
+            height="50%"
+            padding="0%"
+            max-width="100%"
+            className="img-responsive d-inline-block align-top"
+            alt="Mule"
+            overflow="hidden"
+          />
+          <p>
+            An on-demand, hassle-free, item return service that saves you the
+            time and headache from traffic.
+          </p>
         </div>
         <br />
-        <h5 className="loginRegister">Login or Register to sumbit a request</h5>
+        <h5 className="loginRegister">Login or Register to submit a request</h5>
         <Button variant="primary" size="lg" href="/login" block>
           Login
         </Button>
